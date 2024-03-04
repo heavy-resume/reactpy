d014012b0bb4c1653e93d1fd5f9fcc28f109258a
James Hutchison <122519877+JamesHutchison@users.noreply.github.com>
2024-03-04 20:42:07 +0000

    wip

--- src/py/reactpy/reactpy/core/_life_cycle_hook.py
+++ src/py/reactpy/reactpy/core/_life_cycle_hook.py
@@ -170,7 +170,7 @@ class LifeCycleHook:
     def add_state_update(self, updated_state: _CurrentState | Ref) -> None:
         if (
             updated_state.key
-            and self._previous_states[updated_state.key] != updated_state.value
+            and self._previous_states.get(updated_state.key) is not updated_state.value
         ):
             self._updated_states[updated_state.key] = updated_state.value
 
