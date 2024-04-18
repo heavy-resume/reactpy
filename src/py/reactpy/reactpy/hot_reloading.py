c4367ff96de76249ff60b7ae83ba0d5843876713
James Hutchison <122519877+JamesHutchison@users.noreply.github.com>
2024-04-18 04:05:21 +0000

    Add jurigged decorator workaround

--- src/py/reactpy/reactpy/hot_reloading.py
+++ src/py/reactpy/reactpy/hot_reloading.py
@@ -8,6 +8,7 @@ active_connections = []
 
 def monkeypatch_jurigged_to_kill_connections_if_function_update():
     import jurigged.codetools as jurigged_codetools  # type: ignore
+    import jurigged.utils as jurigged_utils  # type: ignore
 
     OrigFunctionDefinition = jurigged_codetools.FunctionDefinition
 
@@ -25,5 +26,13 @@ def monkeypatch_jurigged_to_kill_connections_if_function_update():
                 loop.close()
                 active_connections.clear()
             return super().reevaluate(new_node, glb)
+
+        def stash(self, lineno=1, col_offset=0):
+            if not isinstance(self.parent, OrigFunctionDefinition):
+                co = self.get_object()
+                if co and (delta := lineno - self.node.extent.lineno):
+                    self.recode(jurigged_utils.shift_lineno(co, delta), use_cache=False)
+
+            return super(OrigFunctionDefinition, self).stash(lineno, col_offset)
 
     jurigged_codetools.FunctionDefinition = NewFunctionDefinition
